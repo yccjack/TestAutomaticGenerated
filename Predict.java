b272f6db56b7505c839c7a92c028814db1564c86
MysticalYcc <15155137510@163.com>
2022-01-28 15:49:17 +0800

    Update Predict.java

--- Predict.java
+++ Predict.java
@@ -9,7 +9,7 @@ import auto.model.MapperModel;
  */
 public final class Predict {
 
-    private static final Predict predict = new Predict();
+   private static final Predict predict = new Predict();
 
     public static Predict getPredict() {
         return predict;
@@ -19,12 +19,18 @@ public final class Predict {
 
     }
 
-    public String predictQuoteParam(String paramName,String paramType,String methodName) {
+    public String predictQuoteParam(String paramName, String paramType, String methodName) {
         boolean create = methodName.contains("create") || methodName.contains("new");
         if (paramType.contains(".")) {
             paramType = paramType.substring(paramType.lastIndexOf(".") + 1);
         }
         MapperModel mapperModel = new MapperModel(paramType);
-        return mapperModel.getHitData(paramName,paramType,create);
+        try {
+            String hitData = mapperModel.getHitData(paramName, paramType, create);
+            return hitData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
