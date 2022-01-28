d0b2f42809d869ee6713a7218020b482cdfead5c
MysticalYcc <15155137510@163.com>
2022-01-28 15:51:01 +0800

    Update OtherHandler.java

--- model/OtherHandler.java
+++ model/OtherHandler.java
@@ -5,4 +5,11 @@ package ;
  * @since 2022/1/26
  */
 public class OtherHandler extends MapperModel {
+    protected static final String functionPre="other";
+    @Override
+    protected String dealWithTypeString(String paramName)
+        throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        FunctionHandler functionHandler = new FunctionHandler(functionPre,paramName);
+        return functionHandler.execute();
+    }
 }
